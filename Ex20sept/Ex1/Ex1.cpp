e1100340bf30a6a19c58dd57559f9f4d964f2354
Tudor <tudy.dumi@icloud.com>
2020-09-20 10:48:42 +0300

    Adaugare ex 1

---
+++ Ex20sept/Ex1/Ex1.cpp
@@ -0,0 +1,22 @@
+﻿// Ex1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+//Se dau n numere naturale. Calculați media lor aritmetica.
+//radical(primulnr*aldoilea*altreilea*....*catenrsunt)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, i, x, s = 0;
+	cin >> n; //citesc numarul de numere
+	for (i = 1; i <= n; i++)
+	{
+		cin >> x; //citesc numerele
+		s = s + x;
+	}
+
+	cout << "Media aritmetica este:" << float(s) / float(n);
+
+
+}
+
